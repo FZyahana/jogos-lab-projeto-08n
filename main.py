a80aaf68003b9621a6c6b0dd6fa94ff8fb536efe
fzyahana <fzyahana@gmail.com>
2023-11-14 02:47:49 -0300

    já spawna os obstaculo

--- main.py
+++ main.py
@@ -64,6 +64,9 @@ background1_4 = pygame.transform.scale(background1_4,(scale_background*backgroun
 
 road_png = pygame.image.load("assets/road.png")
 
+fire_png = pygame.image.load("assets/fire.png")
+scale_fire = (160/fire_png.get_height())
+fire_png = pygame.transform.scale(fire_png,(scale_fire*fire_png.get_width(),scale_fire*fire_png.get_height()))
 
 # Velocidade dos backgrounds
 velocidade1 = 1
@@ -72,14 +75,21 @@ velocidade3 = 3
 velocidade4 = 4
 
 # Definir variáveis para o jogo
+dificuldade = 0
 pos = 0
+qtd_obstacles = [30,50,80]
+spd_obstacles = [5,10,15]
 
 # Array de posições dos backgrounds
 background1_x = [0,408,816,1224,1632]
 background2_x = [0,408,816,1224,1632]
 background3_x = [0,408,816,1224,1632]
 background4_x = [0,408,816,1224,1632]
-array_road = [0,1264]
+
+obstacles_x = [(((i*80)+300*i))+1280 for i in range(qtd_obstacles[dificuldade])]
+obstacles_y = [10] * qtd_obstacles[dificuldade]
+array_road = [((i*1264)) for i in range(round(max(obstacles_x)/1264)+1)]
+print(array_road)
 
 # Função pra blitar os cenários
 def blitParallelBackground_indiv(arraypos,img,speed,qtd):
@@ -114,17 +124,24 @@ while executando:
                 if pos != 2:
                     pos += 1
     
-
-
     # Limpar a tela
     tela.fill((0, 0, 0))  # Preencher a tela com a cor preta
 
     # Blitando o fundo da rua
-    for i in range(2):
-        tela.blit(road_png,(array_road[i],240))
-        array_road[i]-=5
-        if array_road[i] <= -1264:
-            array_road[i] = max(array_road)+1264-5
+    for i in range(len(array_road)):
+        if array_road[i] <= largura and array_road[i] >= -1264:
+            tela.blit(road_png,(array_road[i],240))
+        array_road[i]-=spd_obstacles[dificuldade]
+
+    # Blitando os obstaculos
+    for i in range(qtd_obstacles[dificuldade]):
+        if obstacles_x[i] <= largura and obstacles_x[i] >= -80:
+            if obstacles_y[i] == 10:
+                obstacles_y[i] = pos    
+            tela.blit(fire_png,(obstacles_x[i],240+(obstacles_y[i])*160))
+        obstacles_x[i]-=spd_obstacles[dificuldade]
+
+
 
 
     # Definindo posições, img e velocidades por fase
@@ -132,6 +149,7 @@ while executando:
     arrayimg = [background1_1,background1_2,background1_3,background1_4]
     arrayspeed = [velocidade1,velocidade2,velocidade3,velocidade4]
     blitParallelBackground_full(matrixpos,arrayimg,arrayspeed)
+
 
     # Desenhar personagem principal
     tela.blit(sprite_main.image,(0,240+pos*160))
