86e6659d3dde56a9c1e62bcdb1acc9e62aec3622
fzyahana <fzyahana@gmail.com>
2023-11-15 13:17:00 -0300

    fase0 preparada e agora preparando para as demais fases

--- main.py
+++ main.py
@@ -39,77 +39,6 @@ class AnimatedSprite(pygame.sprite.Sprite):
             self.current_image = (self.current_image + 1) % len(self.images)
             self.image = self.images[self.current_image]
 
-
-# Importar a imagem do corredor
-sprite_image_files = ["Run (1).png", "Run (2).png", "Run (3).png", "Run (4).png",
-                      "Run (5).png", "Run (6).png", "Run (7).png", "Run (8).png"]
-for i in range(len(sprite_image_files)):
-    sprite_image_files[i] = "assets\\sprite\\" + sprite_image_files[i]
-sprite_main = AnimatedSprite(sprite_image_files, 0, 0, 40)
-
-
-def setParallelBackground(dificuldade):
-    if dificuldade == 0:
-        # Importar o fundo fase 1
-        # Considerando que todas as imagens de um parallel vão ter mesmo tamanho, vou deixar o código eficiente
-        background1 = pygame.image.load(
-            "assets/parallax-forest-back-trees.png")
-        scale_background = (240/background1.get_height())
-        background1 = pygame.transform.scale(
-            background1, (scale_background*background1.get_width(), scale_background*background1.get_height()))
-
-        # 2
-        background2 = pygame.image.load("assets/parallax-forest-lights.png")
-        background2 = pygame.transform.scale(
-            background2, (scale_background*background2.get_width(), scale_background*background2.get_height()))
-
-        # 3
-        background3 = pygame.image.load(
-            "assets/parallax-forest-middle-trees.png")
-        background3 = pygame.transform.scale(
-            background3, (scale_background*background3.get_width(), scale_background*background3.get_height()))
-
-        # 4
-        background4 = pygame.image.load(
-            "assets/parallax-forest-front-trees.png")
-        background4 = pygame.transform.scale(
-            background4, (scale_background*background4.get_width(), scale_background*background4.get_height()))
-
-        obstacle_png = pygame.image.load("assets/fire.png")
-        scale_fire = (160/obstacle_png.get_height())
-        obstacle_png = pygame.transform.scale(
-            obstacle_png, (scale_fire*obstacle_png.get_width(), scale_fire*obstacle_png.get_height()))
-
-    return background1, background2, background3, background4, obstacle_png
-
-
-road_png = pygame.image.load("assets/road.png")
-menu_png = pygame.image.load("assets/menu.png")
-
-# Velocidade dos backgrounds
-velocidade1 = 1
-velocidade2 = 2
-velocidade3 = 3
-velocidade4 = 4
-
-# Definir variáveis para o jogo
-dificuldade = 0
-pos = 0
-qtd_obstacles = [30, 50, 80]
-spd_obstacles = [15, 30, 45]
-
-
-# Array de posições dos backgrounds
-background1_x = [0, 408, 816, 1224, 1632]
-background2_x = [0, 408, 816, 1224, 1632]
-background3_x = [0, 408, 816, 1224, 1632]
-background4_x = [0, 408, 816, 1224, 1632]
-
-obstacles_x = [(((i*80)+300*i)) +
-               1280 for i in range(qtd_obstacles[dificuldade])]
-obstacles_y = [10] * qtd_obstacles[dificuldade]
-array_road = [((i*1264)) for i in range(round(max(obstacles_x)/1264)+1)]
-
 # Função pra blitar os cenários
 
 
@@ -121,18 +50,81 @@ def blitParallelBackground_indiv(arraypos, img, speed, qtd):
             arraypos[i] = max(arraypos)+408-5
 
 
-def blitParallelBackground_full(matrixpos, arrayimg, arrayspeed):
+def blitParallelBackground_full(backgrounds_x, backgrounds, spd_backgrounds):
 
-    blitParallelBackground_indiv(matrixpos[0], arrayimg[0], arrayspeed[0], 5)
-    blitParallelBackground_indiv(matrixpos[1], arrayimg[1], arrayspeed[1], 5)
-    blitParallelBackground_indiv(matrixpos[2], arrayimg[2], arrayspeed[2], 5)
-    blitParallelBackground_indiv(matrixpos[3], arrayimg[3], arrayspeed[3], 5)
+    blitParallelBackground_indiv(backgrounds_x[0], backgrounds[0], spd_backgrounds[0], 5)
+    blitParallelBackground_indiv(backgrounds_x[1], backgrounds[1], spd_backgrounds[1], 5)
+    blitParallelBackground_indiv(backgrounds_x[2], backgrounds[2], spd_backgrounds[2], 5)
+    blitParallelBackground_indiv(backgrounds_x[3], backgrounds[3], spd_backgrounds[3], 5)
+
+def setParallelBackground(dificuldade):
+    if dificuldade == 0:
+        # Importar o fundo fase 1
+        # Considerando que todas as imagens de um parallel vão ter mesmo tamanho, vou deixar o código eficiente
+        background1 = pygame.image.load(
+            "assets/fase0/parallax-forest-back-trees.png")
+        scale_background = (240/background1.get_height())
+        background1 = pygame.transform.scale(
+            background1, (scale_background*background1.get_width(), scale_background*background1.get_height()))
+
+        # 2
+        background2 = pygame.image.load("assets/fase0/parallax-forest-lights.png")
+        background2 = pygame.transform.scale(
+            background2, (scale_background*background2.get_width(), scale_background*background2.get_height()))
+
+        # 3
+        background3 = pygame.image.load(
+            "assets/fase0/parallax-forest-middle-trees.png")
+        background3 = pygame.transform.scale(
+            background3, (scale_background*background3.get_width(), scale_background*background3.get_height()))
+
+        # 4
+        background4 = pygame.image.load(
+            "assets/fase0/parallax-forest-front-trees.png")
+        background4 = pygame.transform.scale(
+            background4, (scale_background*background4.get_width(), scale_background*background4.get_height()))
+
+        obstacle_png = pygame.image.load("assets/fase0/fire.png")
+        scale_fire = (160/obstacle_png.get_height())
+        obstacle_png = pygame.transform.scale(
+            obstacle_png, (scale_fire*obstacle_png.get_width(), scale_fire*obstacle_png.get_height()))
+        backgrounds = [background1,background2, background3, background4]
+
+    return backgrounds, obstacle_png
+
+# Importar a imagem do corredor
+sprite_image_files = ["Run (1).png", "Run (2).png", "Run (3).png", "Run (4).png",
+                      "Run (5).png", "Run (6).png", "Run (7).png", "Run (8).png"]
+for i in range(len(sprite_image_files)):
+    sprite_image_files[i] = "assets\\sprite\\" + sprite_image_files[i]
+sprite_main = AnimatedSprite(sprite_image_files, 0, 0, 40)
+
+
+road_png = pygame.image.load("assets/road.png")
+menu_png = pygame.image.load("assets/menu.png")
+
+# Definir variáveis para o jogo
+dificuldade = 0
+pos = 0
+qtd_obstacles = [30, 50, 80]
+spd_obstacles = [15, 30, 45]
+
+
+# Array de posições dos backgrounds
+backgrounds_x = [[(x*408) for x in range(5)] for i in range(4)]
+print(backgrounds_x)
+
+obstacles_x = [(((i*80)+300*i)) +
+               1280 for i in range(qtd_obstacles[dificuldade])]
+obstacles_y = [10] * qtd_obstacles[dificuldade]
+array_road = [((i*1264)) for i in range(round(max(obstacles_x)/1264)+1)]
+spd_backgrounds = [1,2,3,4]
+
 
 
 # Defina o título da janela
 pygame.display.set_caption("Projeto 08N JogosLab")
-background1, background2, background3, background4, obstacle_png = setParallelBackground(
-    dificuldade)
+backgrounds, obstacle_png = setParallelBackground(dificuldade)
 # Loop principal do jogo
 menu = True
 executando = True
@@ -189,27 +181,20 @@ while executando:
                 # pygame.time.delay(50)
             obstacles_x[i] -= spd_obstacles[dificuldade]
 
-            if (obstacles_x[qtd_obstacles[dificuldade]-1] < 0) and (dificuldade < 2):
+            if (obstacles_x[qtd_obstacles[dificuldade]-1] < -80) and (dificuldade < 2):
 
                 dificuldade += 1
-                background1, background2, background3, background4, obstacle_png = setParallelBackground(
-                    dificuldade)
+                backgrounds, obstacle_png = setParallelBackground(dificuldade)
+                
                 obstacles_x = [
                     (((i*80)+300*i))+1280 for i in range(qtd_obstacles[dificuldade])]
                 obstacles_y = [10] * qtd_obstacles[dificuldade]
                 array_road = [((i*1264))
                               for i in range(round(max(obstacles_x)/1264)+1)]
-                velocidade1 *= 2
-                velocidade2 *= 2
-                velocidade3 *= 2
-                velocidade4 *= 2
+                spd_backgrounds = [2*i for i in spd_backgrounds]
 
-        # Definindo posições, img e velocidades por fase
-        matrixpos = [background1_x, background2_x,
-                     background3_x, background4_x]
-        arrayimg = [background1, background2, background3, background4]
-        arrayspeed = [velocidade1, velocidade2, velocidade3, velocidade4]
-        blitParallelBackground_full(matrixpos, arrayimg, arrayspeed)
+        
+        blitParallelBackground_full(backgrounds_x, backgrounds, spd_backgrounds)
 
         # Desenhar personagem principal
         tela.blit(sprite_main.image, (0, 240+pos*160))
