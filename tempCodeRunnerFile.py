eb51db67611aa7b52fe3d3fc0289466b4a29bf33
fzyahana <fzyahana@gmail.com>
2023-11-15 11:51:53 -0300

    organizando

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    # for i in range(5):
-    #     tela.blit(background1_1,(background1_x[i],0))
-    #     background1_x[i]-=1
-    #     if background1_x[i] <= -408:
-    #         background1_x[i] = max(background1_x)+408
